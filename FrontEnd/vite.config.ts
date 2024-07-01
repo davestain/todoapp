58097a3f74c572e7278022a3774ffc7d086e6892
David Aceves <deividvengeance@gmail.com>
2024-06-30 23:28:17 -0600

    fixed docker issues

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -6,6 +6,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+     usePolling: true,
+    },
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+  },
   test: {
     environment: 'jsdom',
     globals: true,
